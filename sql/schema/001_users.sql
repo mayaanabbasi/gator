c9e4b0058cd58d01eb74dfc533aee7847cb260dd
Ayaan Abbasi <ayaan.abbasi0@gmail.com>
2026-05-09 18:52:14 +0530

    fix issue with down migration

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -2,9 +2,9 @@
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     created_at DATE NOT NULL,
-    updated_at DATE NOT NULL
-    name TEXT UNIQUE NOT NULL,
+    updated_at DATE NOT NULL,
+    name TEXT UNIQUE NOT NULL
 );
 
--- -goose Down
+-- +goose Down
 DROP TABLE users;
